b48ed5ffe4fd15a6907e63d40b5d3813ac5a0442
poopitscwish <shenya200lalala@mail.ru>
2022-06-12 02:52:11 +0800

    Подсказки на графикке

--- src/main/java/com/example/forecast/HelloController.java
+++ src/main/java/com/example/forecast/HelloController.java
@@ -1,22 +1,5 @@
 package com.example.forecast;
 
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Tooltip;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import org.apache.poi.ss.usermodel.RichTextString;
-
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,76 +7,174 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class HelloController implements Initializable {
+import javax.swing.*;
+
+
+public class HelloController {
 
     @FXML
-    LineChart chart1;
+    private ResourceBundle resources;
+
     @FXML
-    TextFlow text;
+    private URL location;
+
     @FXML
-    TextFlow text2;
+    private MenuItem Open;
+
+    @FXML
+    private NumberAxis X1;
+
+    @FXML
+    private NumberAxis Y1;
+
+    @FXML
+    private LineChart<NumberAxis,NumberAxis> chart1;
+
+    @FXML
+    private TextFlow text;
+
+    @FXML
+    private TextFlow text2;
 
     private XYChart.Series<Number, Number> series;
     private XYChart.Series<Number, Number> series2;
     private ArrayList<Double> curs;
     String textArea;
+    Parsing parsing;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        text.getChildren().clear();//Очистка полей
-        text2.getChildren().clear();
-        chart1.getData().clear();
+    @FXML
+    void initialize() {
+        assert Open != null : "fx:id=\"Open\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert X1 != null : "fx:id=\"X1\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert Y1 != null : "fx:id=\"Y1\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert chart1 != null : "fx:id=\"chart1\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert text != null : "fx:id=\"text\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert text2 != null : "fx:id=\"text2\" was not injected: check your FXML file 'hello-view.fxml'.";
+        chart1.setData(FXCollections.observableArrayList(new XYChart.Series("USD", plot(1,2,3,4,5))));
     }
 
     public void GetData(String path) {
         try {
             text.getChildren().clear();
-            Parsing a = new Parsing(path);
-            a.read();
-            this.curs = new ArrayList<>(a.getData());
-            this.series = new XYChart.Series();
-            for (int i = 0; i < curs.size(); i++) {
-                series.getData().add(new XYChart.Data<>(i, curs.get(i))); //Здесь добавляю к серии значений по Х - даты по У - курс
-                text2.getChildren().add(new Text(i + ") " + a.getTime().get(i) + " - " + curs.get(i) + "\n"));
+            parsing = new Parsing(path);
+            parsing.read();
+            this.curs = new ArrayList<>(parsing.getData());
+            float[] a = new float[curs.size()];
+            float t = 0;
+            for(int i = 0; i<curs.size();i++){
+                t =Float.parseFloat(curs.get(i).toString());
+                a[i] = t;
+            }
+            chart1.setCursor(Cursor.CROSSHAIR);
+            series = new XYChart.Series<>();
+            for(int i = 0; i<curs.size();i++){
+                series.getData().add(new XYChart.Data<>(i,curs.get(i)));
+            }
+            //chart1.setData(FXCollections.observableArrayList(new XYChart.Series("USD", plot(a))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Stat(){
+        Statistics stat = new Statistics(curs);// здесь статистика
+        System.out.println(stat.getMean());
+        text.getChildren().add(new TextFlow(new Text(String.format(
+                "Статистика:\nКоличество:%s\nСумма:%s\nМинимум:%s\nМаксимум:%s\nСреднее:%s\nДисперсия:%s\nСреднее отклонение:%.3f\nМедиана:%.3f",
+                stat.getMean().getCount(), (float) stat.getMean().getSum(),
+                stat.getMean().getMin(), stat.getMean().getMax(),
+                (float) stat.getMean().getAverage(),
+                (float) stat.getVariance(),
+                (float) stat.getStdDev(),
+                stat.median()))));
+    }
+    //Отрисовка графика
+    public void Graph(XYChart.Series series, String name, int code) {
+        series.setName(name);
+        try {
+            chart1.getData().add(series);// добовляю на lInechart серии значений
+            for(var s : chart1.getData())
+            for (XYChart.Data<NumberAxis,NumberAxis> entry: s.getData()) {
+                Tooltip t = new Tooltip(String.format("%.2f",entry.getYValue()));
+                Tooltip.install(entry.getNode(), t);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //Отрисовка графика
-    public void Graph(XYChart.Series series) {
-        chart1.getData().clear();
-        series.setName("USD");
-        try {
-            chart1.setCreateSymbols(true);
-            //chart1.getStylesheets().add(ForeCast.class.getResource("G:\\asd.css").toExternalForm());
-            chart1.getData().addAll(series);// добовляю на lInechart серии значений
-            ObservableList<XYChart.Data> dataList = ((XYChart.Series) chart1.getData().get(0)).getData();
-            dataList.forEach(data -> {
-                Node node = data.getNode();
-                Tooltip tooltip = new Tooltip('(' + data.getXValue().toString() + ';' + data.getYValue().toString() + ')');  //Этот код должен был давать подсказки при наведении на значения, но почему-то не работает. Ссылка откуда брал информацию URL: https://habr.com/ru/post/242009/
-                Tooltip.install(node, tooltip);
+    public ObservableList<XYChart.Data<Integer, Float>> plot(float... y) {
+        final ObservableList<XYChart.Data<Integer, Float>> dataset = FXCollections.observableArrayList();
+        int i = 0;
+        while (i < y.length) {
+            final XYChart.Data<Integer, Float> data = new XYChart.Data<>(i + 1, y[i]);
+            data.setNode(
+                    new HoveredThresholdNode(
+                            (i == 0) ? 0 : y[i-1],
+                            y[i]
+                    )
+            );
+
+            dataset.add(data);
+            i++;
+        }
+
+        return dataset;
+    }
+    class HoveredThresholdNode extends StackPane {
+        HoveredThresholdNode(float priorValue, float value) {
+            setPrefSize(15, 15);
+
+            final Label label = createDataThresholdLabel(priorValue, value);
+
+            setOnMouseEntered(mouseEvent -> {
+                getChildren().setAll(label);
+                setCursor(Cursor.NONE);
+                toFront();
             });
-            Statistics stat = new Statistics(curs);// здесь статистика
-            System.out.println(stat.getMean());
-            text.getChildren().add(new TextFlow(new Text(String.format(
-                    "Статистика:\nКоличество:%s\nСумма:%s\nМинимум:%s\nМаксимум:%s\nСреднее:%s\nДисперсия:%s\nСреднее отклонение:%.3f\nМедиана:%.3f",
-                    stat.getMean().getCount(), (float) stat.getMean().getSum(),
-                    stat.getMean().getMin(), stat.getMean().getMax(),
-                    (float) stat.getMean().getAverage(),
-                    (float) stat.getVariance(),
-                    (float) stat.getStdDev(),
-                    stat.median()))));
-        } catch (Exception e) {
-            e.printStackTrace();
+            setOnMouseExited(mouseEvent -> {
+                getChildren().clear();
+                setCursor(Cursor.CROSSHAIR);
+            });
+        }
+        private Label createDataThresholdLabel(float priorValue, float value) {
+            final Label label = new Label(value + "");
+            label.getStyleClass().addAll("default-color0", "chart-line-symbol", "chart-series-line");
+            label.setStyle("-fx-font-size: 20; -fx-font-weight: bold;");
+
+            if (priorValue == 0) {
+                label.setTextFill(Color.DARKGRAY);
+            } else if (value > priorValue) {
+                label.setTextFill(Color.FORESTGREEN);
+            } else {
+                label.setTextFill(Color.FIREBRICK);
+            }
+
+            label.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+            return label;
         }
     }
-
-
     //Открытие фала по нажатию на кнопку меню 'Open' на главном экране вызывается этот метод
-    public void OpneFile() {
+    public void OpenFile() {
+        chart1.getData().clear();
         textArea = "";
         text2.getChildren().clear();
         final FileChooser fileChooser = new FileChooser();
@@ -105,7 +186,8 @@ public class HelloController implements Initializable {
         }
         System.out.println(textArea);
         GetData(textArea);
-        Graph(series);
+        Graph(series, "USD",0);
+        Stat();
     }
 
     private void printLog(List<File> files) {
@@ -133,7 +215,7 @@ public class HelloController implements Initializable {
 
     //Прогнозирование
     public void predict() {
-        chart1.setCreateSymbols(false);
+        text2.getChildren().clear();
         Statistics stat = new Statistics(curs);
         if (series2 != null)
             chart1.getData().remove(series2);
@@ -182,21 +264,26 @@ public class HelloController implements Initializable {
         for (int i = 0; i < _Y.length; i++) {
             _Y[i] = (float) (_y + b1 * (i - _x));
         }
-
         series2 = new XYChart.Series<>();
         //Отрисовка второго графика
         series.setName("USD");
         series2.setName("Predict");
         Text text = new Text();
         StringBuilder str = new StringBuilder();
+        for (int i = 0; i < curs.size(); i++) {
+            series.getData().add(new XYChart.Data<>(i, curs.get(i))); //Здесь добавляю к серии значений по Х - даты по У - курс
+            text2.getChildren().add(new Text(i + ") " + parsing.getTime().get(i) + " - " + curs.get(i) + "\n"));
+        }
+        text.setText(str.toString());
+        text2.getChildren().add(text);
+        str.delete(0, str.length());
         for (int k = 0; k < curs.size() + line; k++) {
             series2.getData().add(new XYChart.Data<>(k, _Y[k]));
-            if(k>= curs.size())
-            str.append(k + ")" + _Y[k] + "\n");
+            if(k>=curs.size())
+                str.append(k+")"+_Y[k]+"\n");
         }
         text.setFill(javafx.scene.paint.Color.RED);
         text.setText(str.toString());
-        text2.getChildren().add(text);
-        chart1.getData().addAll(series2);
+        Graph(series2,"Predict",1);
     }
 }
