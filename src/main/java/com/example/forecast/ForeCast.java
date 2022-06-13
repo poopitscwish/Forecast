849fe510e36e041baa80ccf972e8b33f4aa40d6e
poopitscwish <shenya200lalala@mail.ru>
2022-06-13 22:18:12 +0800

    .

--- src/main/java/com/example/forecast/ForeCast.java
+++ src/main/java/com/example/forecast/ForeCast.java
@@ -2,26 +2,10 @@ package com.example.forecast;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.javamoney.moneta.*;
 
-import javax.money.CurrencyUnit;
-import javax.money.MonetaryAmount;
-import javax.money.NumberValue;
-import javax.money.convert.CurrencyConversion;
-import javax.money.convert.ExchangeRate;
-import javax.money.convert.ExchangeRateProvider;
-import javax.money.convert.MonetaryConversions;
-import java.awt.*;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Currency;
-import java.util.List;
 
 
 public class ForeCast extends Application {
